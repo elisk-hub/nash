0a4a48edec64f778e56dc91f8aa3626ec69c0b8f
kizowska <kizowska@kth.se>
2026-05-21 16:25:19 +0200

    Updated Salam Ala Qabrin with correct harakat in the end

--- js/nasheed/salamun-ala-qabrin.js
+++ js/nasheed/salamun-ala-qabrin.js
@@ -1,95 +1,95 @@
-const salamAlaQabrin = {
-    id: "salam-ala-qabrin",
-    titleAr: "سَلَامٌ عَلَىٰ قَبْر",
-    titleEn: "Salamun Ala Qabrin",
-    shortTitle: "Salamun Ala Qabrin",
-    info: "A beautiful poem of salutation and yearning for the Prophet ﷺ and his Rawdah",
-    verses: [
-      {
-        type: "chorus",
-        number: "CHORUS",
-        ar: "لَا إِلَـٰهَ إِلَّا اللّٰهْ لَا إِلَـٰهَ إِلَّا اللّٰهْ<br>لَا إِلَـٰهَ إِلَّا اللّٰهْ مُحَمَّدٌ رَسُولُ اللّٰهْ",
-        tl: "Lā ilāha illa Llāh, lā ilāha illa Llāh<br>Lā ilāha illa Llāh, Muḥammadun Rasūlu Llāh",
-        en: "There is no god but Allah, there is no god but Allah<br>There is no god but Allah, Muhammad is the Emissary of Allah"
-      },
-      {
-        type: "verse",
-        number: "VERSE 1",
-        ar: "سَلَامٌ عَلَىٰ قَبْرٍ يُزَارُ مِنَ الْبُعْدْ<br>سَلَامٌ عَلَى الرَّوْضَةْ وَفِيهَا مُحَمَّدْ",
-        tl: "Salāmun ʿalā qabrin yuzāru mina l-buʿd<br>Salāmun ʿalā r-Rawḍa wa fīhā Muḥammad",
-        en: "Peace and greetings to a grave that is visited from afar<br>Peace be upon the Garden wherein lies Muhammad"
-      },
-      {
-        type: "verse",
-        number: "VERSE 2",
-        ar: "سَلَامٌ عَلَىٰ مَنْ زَارَ فِي اللَّيْلِ رَبَّهْ<br>فَبَلَّغَهُ الْمَرْغُوبَ فِي كُلِّ مَقْصَدْ",
-        tl: "Salāmun ʿalā man zāra fī l-layli Rabbah<br>Fa-ballaghahu l-marghūba fī kulli maqṣad",
-        en: "Peace upon him who in the night visited his Lord<br>Who granted him attainment of his desire in all he aspired to"
-      },
-      {
-        type: "verse",
-        number: "VERSE 3",
-        ar: "سَلَامٌ عَلَىٰ مَنْ قَالَ لِلضَّبِّ مَنْ أَنَا<br>فَقَالَ رَسُولُ اللهِ أَنْتَ مُحَمَّدْ",
-        tl: "Salāmun ʿalā man qāla li-ḍ-ḍabbi man anā<br>Fa-qāla Rasūlu Llāhi anta Muḥammad",
-        en: "Peace be upon him who asked a lizard, 'Who am I?'<br>And it replied, 'The Emissary of Allah. You are Muhammad.'"
-      },
-      {
-        type: "verse",
-        number: "VERSE 4",
-        ar: "سَلَامٌ عَلَى الْمَدْفُونِ فِي أَرْضِ طَيْبَةْ<br>وَمَنْ خَصَّهُ الرَّحْمَانُ بِالْفَضْلِ والْمَجْدْ",
-        tl: "Salāmun ʿalā l-madfūni fī arḍi Ṭaybah<br>Wa man khaṣṣahu r-Raḥmānu bi-l-faḍli wa l-majd",
-        en: "Peace upon the one buried in the land of Tayba (Madinah)<br>And whom the All-Merciful singled out for pre-eminence and glory"
-      },
-      {
-        type: "verse",
-        number: "VERSE 5",
-        ar: "نَبِيٌّ حَبَاهُ اللهُ بِالْحُسْنِ وَالْبَهَا<br>فَطُوبَىٰ لِعَبْدٍ زَارَ قَبْرَ مُحَمَّدْ",
-        tl: "Nabiyyun ḥabāhu Llāhu bi-l-ḥusni wa l-bahā<br>Fa-ṭubā li-ʿabdin zāra qabra Muḥammad",
-        en: "A Prophet whom Allah granted beauty and splendour<br>Blessed, then, is any servant who visits the grave of Muhammad"
-      },
-      {
-        type: "verse",
-        number: "VERSE 6",
-        ar: "أَيَا رَاكِبًا نَحْوَ الْمَدِينَةِ قَاصِدًا<br>فَبَلِّغْ سَلَامِي لِلْحَبِيبِ مُحَمَّدْ",
-        tl: "Ayā rākiban naḥwa l-Madِينati qāṣidan<br>Fa-balligh salāmī li-l-ḥabībi Muḥammad",
-        en: "O you who are riding straight ahead towards Madinah<br>Convey my greetings to the Beloved Muhammad"
-      },
-      {
-        type: "verse",
-        number: "VERSE 7",
-        ar: "فِي رَوْضَتِهِ الْحُسْنَىٰ مُنَايَ وَبُغْيَتِي<br>وَفِيهَا شِفَا قَلْبِي وَرُوحِي وَرَاحَتِي",
-        tl: "Fī Rawḍatihi l-ḥusnā munāya wa bughyatī<br>Wa fīhā shifā qalbī wa rūḥī wa rāḥatī",
-        en: "Within his most lovely Garden is my sole desire and goal<br>Therein lies the remedy for my heart and soul, and therein my comfort"
-      },
-      {
-        type: "verse",
-        number: "VERSE 8",
-        ar: "فَإِنْ بَعُدَتْ عَنِّي وَعَزَّ مَزَارُهَا<br>فَتِمْثَالُهَا لَدَيَّ أَحْسەنُ صُورَةْ",
-        tl: "Fa-in baʿudat ʿannī wa ʿazza mazāruhā<br>Fa-timthāluhā ladayya aḥsanu ṣūrah",
-        en: "For although it is far from me and difficult to visit<br>The picture of it in my mind’s eye is the loveliest of forms"
-      },
-      {
-        type: "verse",
-        number: "VERSE 9",
-        ar: "أُنَزِّهُ طَرْفَ الْعَيْنِ فِي حُسْنِ رَوْضِهَا<br>فَيَسْلُو بِهَا لُبِّي وَسِرِّي وَمُهْجَتِي",
-        tl: "Unazzihu ṭarfa l-ʿayni fī ḥusni rawḍihā<br>Fa-yaslū bihā lubbī wa sirrī wa muhjatī",
-        en: "I cleanse my eyesight by picturing the beauty of its gardens<br>So that my mind, being and inmost soul find solace"
-      },
-      {
-        type: "verse",
-        number: "VERSE 10",
-        ar: "فَهَا أَنَا يَا قُطْبَ الْعَوَالِمِ كُلِّهَا<br>أُقَبِّلُهَا شَوْقًا لِإِشْفَاءِ عِلَّتِي",
-        tl: "Fa-hā anā yā Quṭba l-ʿAwālimi kullihā<br>Uqabbiluhā shawqan li-ishfā'i ʿillatī",
-        en: "So here I am, O Axis of the entire Universe!<br>I place my lips on it in yearning, to have my ailment healed"
-      },
-      {
-        type: "verse",
-        number: "VERSE 11",
-        ar: "وَصَلِّ عَلَىٰ قُطْبِ الْوُجُودِ مُحَمَّدْ<br>صَلَاةً بِهَا تَمْحُو عَنَّا كُلَّ زَلَّةْ",
-        tl: "Wa ṣalli ʿalā Quṭbi l-wujūdi Muḥammad<br>Ṣalātan bihā tamḥū ʿannā kulla zallah",
-        en: "Send prayers upon the Centre of all creation, Muhammad<br>Prayers through which all our shortcomings are effaced"
-      }
-    ]
-  };
-  
-  export default salamAlaQabrin;
+const salamunAlaQabrin = {
+  id: "salamun-ala-qabrin",
+  titleAr: "سَلَامٌ عَلَىٰ قَبْرٍ",
+  titleEn: "Salamun Ala Qabrin",
+  shortTitle: "Salamun Ala Qabrin",
+  info: "A deeply moving poem sending peace and yearning salutations to the resting place of the Prophet ﷺ.",
+  verses: [
+    {
+      type: "verse",
+      number: "1",
+      ar: "سَلَامٌ عَلَىٰ قَبْرٍ يُزَارُ مِنَ الْبُعْدِ ❁<br>سَلَامٌ عَلَى الرَّوْضَةِ وَفِيهَا مُحَمَّدٍ ❁",
+      tl: "Salāmun ʿalā qabrin yuzāru mina l-buʿdi.<br>Salāmun ʿalā r-Rawḍati wa fīhā Muḥammadin.",
+      en: "Peace and greetings to a grave that is visited from afar.<br>Peace be upon the Garden wherein lies Muhammad."
+    },
+    {
+      type: "verse",
+      number: "2",
+      ar: "سَلَامٌ عَلَىٰ مَنْ زَارَ فِي اللَّيْلِ رَبَّهُ ❁<br>فَبَلَّغَهُ الْمَرْغُوبَ فِي كُلِّ مَقْصَدٍ ❁",
+      tl: "Salāmun ʿalā man zāra fī l-layli Rabbahu,<br>Fa-ballaghahu l-marghūba fī kulli maqṣadin.",
+      en: "Peace upon him who in the night visited his Lord,<br>Who granted him attainment of his desire in all he aspired to."
+    },
+    {
+      type: "verse",
+      number: "3",
+      ar: "سَلَامٌ عَلَىٰ مَنْ قَالَ لِلضَّبِّ مَنْ أَنَا ❁<br>فَقَالَ رَسُولُ اللهِ أَنْتَ مُحَمَّدٍ ❁",
+      tl: "Salāmun ʿalā man qāla li-ḍ-ḍabbi man anā.<br>Fa-qāla Rasūlu Llāhi anta Muḥammadin.",
+      en: "Peace be upon him who asked a lizard, 'Who am I?'<br>And it replied, 'The Emissary of Allah. You are Muhammad.'"
+    },
+    {
+      type: "verse",
+      number: "4",
+      ar: "سَلَامٌ عَلَى الْمَدْفُONِ فِي أَرْضِ طَيْبَةَ ❁<br>وَمَنْ خَصَّهُ الرَّحْمَانُ بِالْفَضْلِ والْمَجْدِ ❁",
+      tl: "Salāmun ʿalā l-madfūni fī arḍi Ṭaybata.<br>wa man khaṣṣahu r-Raḥmānu bi-l-faḍli wa l-majdi.",
+      en: "Peace upon the one buried in the land of Tayba (Madinah).<br>and whom the All-Merciful singled out for pre-eminence and glory."
+    },
+    {
+      type: "verse",
+      number: "5",
+      ar: "نَبِيٌّ حَبَاهُ اللهُ بِالْحُسْنِ وَالْبَهَا ❁<br>فَطُوبَىٰ لِعَبْدٍ زَارَ قَبْرَ مُحَمَّدٍ ❁",
+      tl: "Nabiyyun ḥabāhu Llāhu bi-l-ḥusni wa l-bahā.<br>Fa-ṭubā li-ʿabdin zāra qabra Muḥammadin.",
+      en: "A Prophet whom Allah granted beauty and splendour.<br>Blessed, then, is any servant [of Allah] who visits the grave of Muhammad."
+    },
+    {
+      type: "verse",
+      number: "6",
+      ar: "أَيَا رَاكِبًا نَحْوَ الْمَدِينَةِ قَاصِدًا ❁<br>فَبَلِّغْ سَلَامِي لِلْحَبِيبِ مُحَمَّدٍ ❁",
+      tl: "Ayā rākiban naḥwa l-Madīnati qāṣidan.<br>fa-balligh salāmī li-l-ḥabībi Muḥammadin.",
+      en: "O you who are riding straight ahead towards Madinah,<br>convey my greetings to the Beloved Muhammad."
+    },
+    {
+      type: "verse",
+      number: "7",
+      ar: "فِي رَوْضَتِهِ الْحُسْنَىٰ مُنَايَ وَبُغْيَتِي ❁<br>وَفِيهَا شِفَا قَلْبِي وَرُوحِي وَرَاحَتِي ❁",
+      tl: "Fī Rawḍatihi l-ḥusnā munāya wa bughyatī,<br>wa fīhā shifā qalbī wa rūḥī wa rāḥatī.",
+      en: "Within his most lovely Garden is my sole desire and goal,<br>therein lies the remedy for my heart and soul, and therein my comfort."
+    },
+    {
+      type: "verse",
+      number: "8",
+      ar: "ف...إِنْ بَعُدَتْ عَنِّي وَعَزَّ مَزَارُهَا ❁<br>فَتِمْثَالُهَا لَدَيَّ أَحْسَنُ صُورَةٍ ❁",
+      tl: "Fa-in baʿudat ʿannī wa ʿazza mazāruhā,<br>fa-timthāluhā ladayya aḥsanu ṣūratin.",
+      en: "For although it is far from me and difficult to visit,<br>the picture of it in my mind’s eye is the loveliest of forms."
+    },
+    {
+      type: "verse",
+      number: "9",
+      ar: "أُنَزِّهُ طَرْفَ الْعَيْنِ فِي حُسْنِ رَوْضِهَا ❁<br>فَيَسْلُو بِهَا لُبِّي وَسِرِّي وَمُهْجَتِي ❁",
+      tl: "Unazzihu ṭarfa l-ʿayni fī ḥusni rawḍihā,<br>fa-yaslū bihā lubbī wa sirrī wa muhjatī.",
+      en: "I cleanse my eyesight by picturing the beauty of its gardens,<br>so that my mind, being and inmost soul find solace."
+    },
+    {
+      type: "verse",
+      number: "10",
+      ar: "فَهَا أَنَا يَا قُطْبَ الْعَوَالِمِ كُلِّهَا ❁<br>أُقَبِّلُهَا شَوْقًا لِإِشْفَاءِ عِلَّتِي ❁",
+      tl: "Fa-hā anā yā Quṭba l-ʿAwālimi kullihā.<br>Uqabbiluhā shawqan li-ishfā'i ʿillatī.",
+      en: "So here I am, O Axis of the entire Universe!<br>I place my lips on it in yearning, to have my ailment healed."
+    },
+    {
+      type: "verse",
+      number: "11",
+      ar: "وَصَلِّ عَلَىٰ قُطْبِ الْوُجُودِ مُحَمَّدٍ ❁<br>صَلَاةً بِهَا تَمْحُو عَنَّا كُلَّ زَلَّةٍ ❁",
+      tl: "Wa ṣalli ʿalā Quṭbi l-wujūdi Muḥammadin,<br>Ṣalātan bihā tamḥū ʿannā kulla zallatin.",
+      en: "Send prayers upon the Centre of all creation, Muhammad,<br>prayers through which all our shortcomings are effaced."
+    },
+    {
+      type: "chorus",
+      number: "12",
+      ar: "لَا إِلَٰهَ إِلَّا اللَّهُ لَا إِلَٰهَ إِلَّا اللَّهُ ❁<br>لَا إِلَٰهَ إِلَّا اللَّهُ مُحَمَّدٌ رَسُولُ اللَّهِ ❁",
+      tl: "Lā ilāha illa Llāh, lā ilāha illa Llāh.<br>Lā ilāha illa Llāh, Muḥammadun Rasūlu Llāh.",
+      en: "There is no god but Allah, there is no god but Allah.<br>There is no god but Allah, Muhammad is the Emissary of Allah."
+    }
+  ]
+};
+
+export default salamunAlaQabrin;
