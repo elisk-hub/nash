d2ad1c793c9f168dda3522e72fda6bd2d2d048e7
kizowska <kizowska@kth.se>
2026-05-22 18:17:54 +0200

    Added new nasheed

---
+++ js/nasheed/anta-fina.js
@@ -0,0 +1,18 @@
+const antaFina = {
+    id: "anta-fina",
+    titleAr: "أَنْتَ فِيْنَا",
+    titleEn: "Anta Fina",
+    shortTitle: "Anta Fina",
+    info: "En vacker andlig dikt (Qasida).",
+    verses: [
+      { type: "chorus", ar: "أَنْتَ فِيْنَا أَنْتَ فِيْنَا<br>لَا نَعْذِبُ يَا حَبِيْبِي وَ أَنْتَ فِيْنَا", tl: "Anta fina, anta fina<br>La na'dhibu ya habibi wa anta fina" },
+      { type: "verse", ar: "كُلُّ مَنْ زَارَ الْمَقَامَ فَالنَّبِي رَدَّ السَّلَام<br>يَعْرِفُ الْخَلْقَ تَمَامَا أَبْشِرُوا زَوَّارَ نَبِيْنَا", tl: "Kullu man zara-l-maqama fan-nabi radda-s-salam<br>ya'rifu-l-khalqa tamama abshiru zuwwara nabina" },
+      { type: "chorus", ar: "أَنْتَ فِيْنَا أَنْتَ فِيْنَا<br>لَا نَعْذِبُ يَا حَبِيْبِي وَ أَنْتَ فِيْنَا", tl: "Anta fina, anta fina<br>La na'dhibu ya habibi wa anta fina" },
+      { type: "verse", ar: "وَجْهُهُ فَاقَ الْبُدُورَا زَادَهُ الْمَوْلَى سُرُورَا<br>قَدْ بَدَا فِي الْكَوْنِ نُورُ خَلْقِ اللهِ نَبِيْنَا", tl: "Wajhuhu faqa-l-budura zadahu-l-mawla surura<br>qad bada fi-l-kawni nuru khalqi-l-lahi nabina" },
+      { type: "chorus", ar: "أَنْتَ فِيْنَا أَنْتَ فِيْنَا<br>لَا نَعْذِبُ يَا حَبِيْبِي وَ أَنْتَ فِيْنَا", tl: "Anta fina, anta fina<br>La na'dhibu ya habibi wa anta fina" },
+      { type: "verse", ar: "خَيْرُ خَلْقِ اللهِ طَه مِثْلَ شَمْسٍ فِي ضُحَاهَا<br>هَذِهِ الدُّنْيَا مَرَاهَا فِي ضِيَاءِ مَنْ نَبِيْنَا .. مَنْ نَبِيْنَا", tl: "Khayru khalqi-l-lahi Taha mithla shamsin fi duhaha<br>hadhihi-d-dunya maraha fi diya'i man nabina.. man nabina" },
+      { type: "chorus", ar: "أَنْتَ فِيْنَا أَنْتَ فِيْنَا<br>لَا نَعْذِبُ يَا حَبِيْبِي وَ أَنْتَ فِيْنَا", tl: "Anta fina, anta fina<br>La na'dhibu ya habibi wa anta fina" }
+    ]
+  };
+  
+  export default antaFina;
---
+++ js/nasheed/sallalahu-ala-muhammad.js
@@ -0,0 +1,36 @@
+const sallallahuAlaMuhammad = {
+    id: "sallallahu-ala-muhammad",
+    titleAr: "صَلَّى اللهُ عَلَى مُحَمَّد",
+    titleEn: "Sallallahu Ala Muhammad",
+    shortTitle: "Sallallahu Ala",
+    info: "En vacker hälsning över Profeten Muhammad (fred vare över honom).",
+    verses: [
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "صَلُّوا عَلَى الْهَادِي تَنْجُو<br>مِنْ حَرِّ لَظَى وَيَزُولُ الْهَمَّ", tl: "Sallu 'alal-hadi tanju<br>min harri lazha wa yazulul hamm" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "شَرَّفَ مَكَّةَ لَمَّا أَتَاهَا<br>ثُمَّ النُّورُ عَلَاهَا وَعَمَّ", tl: "Sharrafa Makkata lamma ataha<br>thumma-nnuru 'alaha wa 'amma" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "جِذْعُ النَّخْلِ لَطَه شَكَى<br>ذِرَاعُ الشَّاةِ لَهُ كَلَّمْ", tl: "Jiz'un-nakhli li-Taha shaka<br>zira'ush-shati lahu kallam" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "عَيْنُ قَتَادَةَ لَا نَنْسَاهَا<br>صَارَتْ تُبْصِرُ أَيْنَ وَكَمْ", tl: "'Aynu Qatadata la nansaha<br>sarat tubsiru ayna wa kam" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "سَلَامِي عَلَى طَه<br>سَلَامِي عَلَى الْمَمْدُوحِ بِالنُّونِ وَالْقَلَمْ", tl: "Salami 'ala Taha<br>Salami 'alal-mamduhi bin-nuni wal-qalam" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "سَلَامِي عَلَى مَنْ قَالَ<br>بَانِي رَسُولُ الله", tl: "Salami 'ala man qala<br>bani Rasulullah" },
+      { type: "verse", ar: "لِلنَّاقَةِ اشْهَدِي<br>قَالَتْ لَهُ نَعَمْ", tl: "Lin-naqati ish-hadi<br>qalat lahu na'am" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "سَلَامِي عَلَى مَنْ سَارَ<br>وَكَانَ لَهُ جِبْرِيل", tl: "Salami 'ala man sara<br>wa kana lahu Jibril" },
+      { type: "verse", ar: "لَيْلًا إِلَى الْعَلَا<br>مِنْ أَطْوَعِ الْخَدَمْ", tl: "Laylan ilal-'ala<br>min atwa'il khadam" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "سَلَامِي عَلَى مَنْ قَالَ<br>أَلَا فَانْقَسَمْ قِسْمَيْن", tl: "Salami 'ala man qala<br>ala fanqasam qismayn" },
+      { type: "verse", ar: "لِلْبَدْرِ فِي السَّمَاءِ<br>يَا بَدْرُ فَانْقَسِمْ", tl: "Lil-badri fis-sama'i<br>ya badru fanqasim" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "سَلَامِي عَلَى مَنْ قَالَ<br>أَجْرِهَا مِنَ النِّيرَان", tl: "Salami 'ala man qala<br>ajriha minan-niran" },
+      { type: "verse", ar: "يَا رَبِّ أُمَّتِي<br>قَالَ لَهُ نَعَمْ", tl: "Ya Rabbi ummati<br>qala lahu na'am" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" },
+      { type: "verse", ar: "صَلَّى اللهُ عَلَى مُحَمَّد<br>صَلَّى اللهُ عَلَيْهِ وَسَلَّم", tl: "Sallallahu 'ala Muhammad<br>Sallallahu 'alayhi wasallam" }
+    ]
+  };
+  
+  export default sallallahuAlaMuhammad;
